b9de74ddf871df4957f23b1ce8303a514768fe9a
GitForGood <oliversofta@gmail.com>
2025-05-01 19:01:07 +0200

    Initial Startup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CharacterSheetApp from './components/CharacterSheetApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CharacterSheetApp />
+  </React.StrictMode>
+);
